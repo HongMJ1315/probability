8f40ed08e9fb4e2e7c17b3e7c4a1b41fe5674ece
HongMJ1315 <gone_co_1008@yahoo.com.tw>
2023-01-09 10:49:15 +0800

    ٩(˃̶͈̀௰˂̶͈́)و

--- chart/bainomial.cpp
+++ chart/bainomial.cpp
@@ -14,25 +14,41 @@ int main(){
 
         binomial_distribution<> d(mxn, p);
 
-int hist[mxn + 1] = {};
-memset(hist, 0, sizeof(hist));
-VideoWriter video("bainomial" + to_string(i) + ".mp4", VideoWriter::fourcc('M', 'P', '4', 'V'), 60, Size(Width, Height));
-for(int n = 0; n < Height * 10; ++n){
-    int ran = d(gen);
-    ++hist[ran];
-    if(n % 10 == 0){
-        Mat img(Height, Width, CV_8UC3, Scalar(255, 255, 255));
-        for(int i = 0; i <= mxn; i++){
-            int x = i * Width / (mxn + 1);
-            rectangle(img, Point(x, Height), Point(x + Width / (mxn + 1), Height - min(hist[i], Height)), Scalar(23, 17, 13), -1);
+        int hist[mxn + 1] = {};
+        memset(hist, 0, sizeof(hist));
+        VideoWriter video("bainomial" + to_string(i) + ".mp4", VideoWriter::fourcc('M', 'P', '4', 'V'), 60, Size(Width, Height));
+        for(int n = 0; n < Height * 10; ++n){
+            int ran = d(gen);
+            ++hist[ran];
+            if(n % 10 == 0){
+                Mat img(Height, Width, CV_8UC3, Scalar(255, 255, 255));
+                for(int i = 0; i <= mxn; i++){
+                    int x = i * Width / (mxn + 1);
+                    rectangle(img, Point(x, Height), Point(x + Width / (mxn + 1), Height - min(hist[i], Height)), Scalar(23, 17, 13), -1);
+                }
+                //加入橫坐標
+                for(int i = 0; i <= mxn; i++){
+                    if(i % 10 == 0){
+                        int x = i * Width / (mxn + 1);
+                        line(img, Point(x, Height), Point(x, Height - 10), Scalar(0, 0, 255), 1);
+                        putText(img, to_string(i), Point(x, Height - 10), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
+                    }
+                }
+                video << img;
+            }
         }
-        video << img;
-    }
-}
         for(int j = 0; j < 60; j++){
+            Mat img(Height, Width, CV_8UC3, Scalar(255, 255, 255));
             for(int i = 0; i <= mxn; i++){
                 int x = i * Width / (mxn + 1);
                 rectangle(img, Point(x, Height), Point(x + Width / (mxn + 1), Height - min(hist[i], Height)), Scalar(23, 17, 13), -1);
+            }
+            for(int i = 0; i <= mxn; i++){
+                if(i % 10 == 0){
+                    int x = i * Width / (mxn + 1);
+                    line(img, Point(x, Height), Point(x, Height - 10), Scalar(0, 0, 255), 1);
+                    putText(img, to_string(i), Point(x, Height - 10), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
+                }
             }
             video << img;
         }
